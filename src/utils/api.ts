366ab5ba9f57c6a15dc5ded5ade6f6c4365d2ef4
John Gerald Agbayani <agbayani.johngerald@gmail.com>
2022-03-26 23:37:16 +0800

    Handle server error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,39 +1,44 @@
 // https://exchangeratesapi.io/
-const BASE_URL = 'http://api.exchangeratesapi.io/v1';
+const BASE_URL = 'http://api.exchangeratesapi.io/v1'
 const API_KEY = 'REPLACE_WITH_YOUR_API_KEY'
 
 // TODO: what is the response type in the Promise? We should avoid using 'any'
 type API = (params: {
-  endpoint: string,
+  endpoint: string
   params: {
-    base?: string,
-  },
-}) => Promise<any>;
+    base?: string
+  }
+}) => Promise<any>
 
 const api: API = ({ endpoint, params = {} }) => {
-  const searchParams = new URLSearchParams(params);
-  searchParams.append('access_key', API_KEY);
-  const queryString = searchParams.toString();
+  const searchParams = new URLSearchParams(params)
+  searchParams.append('access_key', API_KEY)
+  const queryString = searchParams.toString()
 
-  return fetch(`${BASE_URL}${endpoint}?${queryString}`);
-};
+  return fetch(`${BASE_URL}${endpoint}?${queryString}`)
+}
 
 export const fetchRates = async (baseCurrency: string) => {
   try {
-    const response = await api({ endpoint: '/latest', params: { base: baseCurrency } });
-    const responseText = await response.text();
-    const { rates, error } = JSON.parse(responseText);
+    const response = await api({
+      endpoint: '/latest',
+      params: { base: baseCurrency },
+    })
+    const responseText = await response.text()
+    const { rates, error } = JSON.parse(responseText)
 
-    if (error) {
-      throw new Error(error);
+    if (error?.code === 'invalid_base_currency') {
+      throw new Error(`Base '${baseCurrency}' is not supported.`)
+    } else if (error) {
+      throw new Error(error)
     }
 
     if (!rates || !Object.keys(rates).length) {
-      throw new Error('Could not fetch rates.');
+      throw new Error('Could not fetch rates.')
     }
 
-    return rates;
+    return rates
   } catch (errorResponse) {
-    throw errorResponse;
+    throw errorResponse
   }
-};
+}
